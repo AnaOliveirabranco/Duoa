22a7ce5aef71a0438195a962dcc629a16955af11
AnaOliveirabranco <oliveirabrancoana@gmail.com>
2026-05-26 09:07:00 -0300

    Adiciona novos dados
    Adiciona novos dados à tabela para fins de testes futuros

--- popbd.sql
+++ popbd.sql
@@ -13,7 +13,10 @@ VALUES ('Bolo no Pote Maracujá',201, 6, 8.50, 60),
 INSERT INTO Cliente(Nome,Cpf,DataNasc,Endereço)
 VALUES ('Alana Oliveira', 98765432191, '20050225','Rua abc de junior 2'),
 	('Regiana  Vilva', 90065432191, '20060201','Rua Castro 120'),
-	('Alana Oliveira', 98765432129, '20021225','Rua Abreu Junior 2019');
+	('Alana Oliveira', 98765432129, '20021225','Rua Abreu Junior 2019'),
+	('Barbara antos', 98765432151, '20000225','Rua Cardoso 22'),
+	('Eloiza Castro', 90065432181, '20010201','Rua Alarcar 100'),
+	('Alana Oliveira', 98765432139, '20031225','Rua Junior Res 209');
 
 
 
@@ -21,4 +24,8 @@ INSERT INTO CompraCL (CodProduto, CodCliente, DataCompra,VLVenda, QtCompra)
 VALUES (  103 ,103,'20260501',13.60, 1),
 	( 103 ,104,'20260502',13.60, 3),
 	( 104 ,105,'20260505',13.60, 2),
-	( 105 ,103,'20260504',13.60, 4);
+	( 105 ,103,'20260504',13.60, 4),
+	( 103 ,106,'20260524',13.60, 1),
+	( 103 ,107,'20260520',13.60, 1),
+	( 104 ,107,'20260521',13.60, 1),
+	( 105 ,108,'20260522',13.60, 1);
