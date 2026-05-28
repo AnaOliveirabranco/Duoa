5ff8835847530a785e7cb5a57d01822fe5f18d02
AnaOliveirabranco <oliveirabrancoana@gmail.com>
2026-05-28 08:22:38 -0300

    Atualizar valores
    Atualização dos valores de um único campo da coluna da tabela e de todos os campos da coluna da tabela.

---
+++ altera.sql
@@ -0,0 +1,10 @@
+UPDATE Produto --Atualiza o valor do lucro apenas de um unico campo da coluna 
+SET Lucro = 60.0
+WHERE CodProduto = 100;
+
+
+UPDATE Produto  --Atualiza o valor do lucro de todos os campos da coluna
+SET Lucro = 84.7;
+
+
+
