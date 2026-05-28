f77f22c74669c06afcab94d6c6722380a790e511
AnaOliveirabranco <oliveirabrancoana@gmail.com>
2026-05-28 10:03:45 -0300

    atualização de nomes de clientes

--- altera.sql
+++ altera.sql
@@ -7,7 +7,15 @@ UPDATE Produto  --Atualiza o valor do lucro de todos os campos da coluna
 SET Lucro = 84.7;
 
 
-
 UPDATE CompraCL
 SET  CodCliente = 103, QtCompra = 3
 WHERE CodCompra = 1006;
+
+
+UPDATE Cliente
+SET  Nome = 'Annie'
+WHERE CodCliente = 105;
+
+UPDATE Cliente
+SET  Nome = 'Filomena'
+WHERE CodCliente = 108;
